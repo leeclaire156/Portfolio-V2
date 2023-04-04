4be6c30b64182bda3b296c983e0cf79f9c85727b
Claire Lee <lee.claire156@gmail.com>
2023-04-04 18:12:19 -0400

    Adjusts navbar and pages to my needs

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className='Home Body'>
+      <h1>Home Page</h1>
+      <p>
+        This portfolio belongs to Claire Lee.
+      </p>
+    </div>
+  );
+}
