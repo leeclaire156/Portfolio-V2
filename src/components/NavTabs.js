35a3cdda76a8c7568b2e7b72a1eb1184cf150e66
Claire Lee <lee.claire156@gmail.com>
2023-04-05 17:22:20 -0400

    Fixes resume download button to allow for resume to be downloaded

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -7,34 +7,35 @@ export default function NavTabs({ currentPage, handlePageChange }) {
       <Navbar collapseOnSelect expand='lg'>
         <Container>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <h1>Claire Lee</h1>
           <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className="me-auto">
-            <Nav.Link href='#home'
-              onClick={() => handlePageChange('Home')}
-              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
-              Home
-            </Nav.Link>
-            <Nav.Link href='#about'
-              onClick={() => handlePageChange('About')}
-              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
-              About
-            </Nav.Link>
-            <Nav.Link href='#portfolio'
-              onClick={() => handlePageChange('Portfolio')}
-              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
-              Portfolio
-            </Nav.Link>
-            <Nav.Link href='#contact'
-              onClick={() => handlePageChange('Contact')}
-              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
-              Contact
-            </Nav.Link>
-            <button className="button">
-              <a href="../assets/Lee_Claire_Resume.pdf" download="Lee_Claire_Resume.pdf">
-                Download Resume
-              </a>
-            </button>
-          </Nav>
+            <Nav className="me-auto">
+              <Nav.Link href='#home'
+                onClick={() => handlePageChange('Home')}
+                className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
+                Home
+              </Nav.Link>
+              <Nav.Link href='#about'
+                onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+                About
+              </Nav.Link>
+              <Nav.Link href='#portfolio'
+                onClick={() => handlePageChange('Portfolio')}
+                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                Portfolio
+              </Nav.Link>
+              <Nav.Link href='#contact'
+                onClick={() => handlePageChange('Contact')}
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                Contact
+              </Nav.Link>
+              <button className="button">
+                <a href={require("./resumes/Lee_Claire_Resume.pdf")} download>
+                  Download Resume
+                </a>
+              </button>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
